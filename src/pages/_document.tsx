6930916ac9ab3394a4504e30c1a29c7e09cd30a6
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-12-26 22:10:41 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,28 +14,26 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="msapplication-TileColor" content="#025aa1" />
         <meta name="theme-color" content="#ffffff" />
-
-        <meta name="application-name" content="Hyperlane Warp Template UI" />
         <meta
           name="keywords"
-          content="Hyperlane Warp Route Template UI Token Bridge Interchain App"
+          content="Cross-Chain Token Bridge"
         />
         <meta
           name="description"
-          content="A web app template for building Hyperlane Warp Route UIs"
+          content="Cross-Chain Token Bridge for JIBChain"
         />
 
         <meta name="HandheldFriendly" content="true" />
-        <meta name="apple-mobile-web-app-title" content="Hyperlane Warp Template UI" />
+        <meta name="apple-mobile-web-app-title" content="Cross-Chain Token Bridge for JIBChain" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
 
-        <meta property="og:url" content="https://hyperlane.xyz" />
-        <meta property="og:title" content="Hyperlane Warp Template UI" />
+        <meta property="og:url" content="http://bridge.jbcha.in/" />
+        <meta property="og:title" content="Cross-Chain Token Bridge for JIBChain" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://hyperlane.xyz/logo.png" />
+        <meta property="og:image" content="http://bridge.jbcha.in/" />
         <meta
           property="og:description"
-          content="A web app template for building Hyperlane Warp Route UIs"
+          content="Cross-Chain Token Bridge for JIBChain"
         />
       </Head>
       <body className="text-black">
