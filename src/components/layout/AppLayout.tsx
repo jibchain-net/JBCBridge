c2e99ae7fafa3a5604a2b3f4531d57eebe89ee99
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-12-26 22:06:09 +0700

    Update AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { PropsWithChildren } from 'react';
 
-import Planet1 from '../../images/planets/planet-1.webp';
-import Planet2 from '../../images/planets/planet-2.webp';
 import { Footer } from '../nav/Footer';
 import { Header } from '../nav/Header';
 
@@ -11,20 +8,17 @@ export function AppLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Head>
-        {/* https://nextjs.org/docs/messages/no-document-viewport-meta */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hyperlane Nexus Bridge</title>
+        <title>JBCBridge | Cross-Chain Token Bridge for JIBChain</title>
       </Head>
       <div
         style={styles.container}
         id="app-content"
-        className="relative flex flex-col justify-between h-full min-h-screen w-full min-w-screen bg-blue-500"
+        className="relative flex flex-col justify-between h-full min-h-screen w-full min-w-screen bg-gray-50"
       >
         <div className="hidden md:flex absolute left-[8%] top-[15%]">
-          <Image src={Planet1} alt="Planet 1" width={200} priority={false} quality={50}></Image>
         </div>
         <div className="hidden md:flex absolute right-[8%] bottom-1/4">
-          <Image src={Planet2} alt="Planet 2" width={220} priority={false} quality={50}></Image>
         </div>
         <Header />
         <div className="sm:px-4 mx-auto grow flex items-center max-w-screen-xl">
