2d21ab1684cffa188ab78d6860779be92544afad
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-01-13 22:37:11 +0700

    Update tokens.ts
    Add tokens
    - Arbitrum One
    ETH Native
    ARB
    
    - Meta Chain
    GASH
    
    - Binance Smart Chain
    BTCB
    CLASS
    
    - Jib Chain
    Native > Arbitrum One > Meta Chain

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -24,16 +24,6 @@ export const tokenList: WarpTokenConfig = [
     hypNativeAddress: '0xd63cD9ab38AeFDa71C87f09E5d78cbd135c442B0',
     logoURI: '/bkc.png',
   },
-  { // bkc > jbc- = CMM (ERC-20)
-    type: 'collateral',
-    chainId: 96,
-    address: '0x9B005000A10Ac871947D99001345b01C1cEf2790',
-    hypCollateralAddress: '0x50040B2f285811e1eeEC1C016D6048d43E963058',
-    name: 'Commu Token',
-    symbol: 'CMM',
-    decimals: 18,
-    logoURI: '/cmm.png',
-  },
   { // 3.bsc > jbc- = native
     type: 'native',
     chainId: 56,
@@ -55,14 +45,43 @@ export const tokenList: WarpTokenConfig = [
   { // 5.op > jbc- = native
     type: 'native',
     chainId: 10,
-    name: 'Ether',
+    name: 'Optimistic',
     symbol: 'ETH',
     decimals: 18,
     hypNativeAddress: '0x18cdA25dB36187106050D41289aCf35c1e78c80d',
     logoURI: '/eth.png',
+  },    
+  // + Add new chain = native
+  { // 6. jbc > meta = native
+    type: 'native',
+    chainId: 8899,
+    name: 'JBC',
+    symbol: 'JBC',
+    decimals: 18,
+    hypNativeAddress: '0xE23Be4D1a1CDA5454e0e82099A75B13AF53cb673',
+    logoURI: '/jbc-2.png',
+  },
+  { // 7. arb > jbc = native (eth)
+    type: 'native',
+    chainId: 42161,
+    name: 'Arbitrum One',
+    symbol: 'ETH',
+    decimals: 18,
+    hypNativeAddress: '0x89C94A3a822686C69A0EDB9E130ff1c0C9A13753',
+    logoURI: '/eth.png',
+  },
+  { // 8. jbc > arb = native
+    type: 'native',
+    chainId: 8899,
+    name: 'JBC',
+    symbol: 'JBC',
+    decimals: 18,
+    hypNativeAddress: '0xc5b66604d72dA3cE864a826aaFc23b988528e8bF',
+    logoURI: '/jbc-2.png',
   },
 
-  // ERC-20
+
+  // Token ERC-20
   { // 1.bsc > jbc = usdt
     type: 'collateral',
     chainId: 56,
@@ -93,6 +112,58 @@ export const tokenList: WarpTokenConfig = [
     decimals: 18,
     logoURI: '/op.png',
   },
+  //
+  { // 4.bkc > jbc- = CMM (ERC-20)
+    type: 'collateral',
+    chainId: 96,
+    address: '0x9B005000A10Ac871947D99001345b01C1cEf2790',
+    hypCollateralAddress: '0x50040B2f285811e1eeEC1C016D6048d43E963058',
+    name: 'Commu Token',
+    symbol: 'CMM',
+    decimals: 18,
+    logoURI: '/cmm.png',
+  },
+  { // 5.bsc > jbc- = BTCB (ERC-20)
+    type: 'collateral',
+    chainId: 56,
+    address: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c',
+    hypCollateralAddress: '0x5833d96F21594be4dd2F4b63C51Ba7C7227918f0',
+    name: 'Binance-Peg BTCB Token',
+    symbol: 'BTCB',
+    decimals: 18,
+    logoURI: '/btc.png',
+  },
+  { // 6.meta > jbc = gash (erc-20)
+    type: 'collateral',
+    chainId: 17,
+    address: '0x4dD1d9Dd0f3d9C3F6F747F99928C29924A1b42b1',
+    hypCollateralAddress: '0xA8C84540c5893ADa365fCe0a5A6B0318EAd72893',
+    name: 'GASH Token',
+    symbol: 'GASH',
+    decimals: 18,
+    logoURI: '/erc20.png',
+  },
+  { // 7.arb > jbc = arb (erc-20)
+    type: 'collateral',
+    chainId: 42161,
+    address: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+    hypCollateralAddress: '0x9cee5Fa6182e1Ea883903051fb014f080098B10b',
+    name: 'ARB',
+    symbol: 'ARB',
+    decimals: 18,
+    logoURI: '/arb.png',
+  },
+  { // 8.bsc > jbc = class (erc-20)
+    type: 'collateral',
+    chainId: 56,
+    address: '0xe57EDc546Ee99f17a5d6e32C2457B4569ecD40f8',
+    hypCollateralAddress: '0xC26b01D78bc57B9dB38D3D2a60BbC6c0e1D2c554',
+    name: 'Class Coin',
+    symbol: 'CLASS',
+    decimals: 18,
+    logoURI: '/erc20.png',
+  },
+
 
   // Example NFT (ERC721) token for an EVM chain
   // {
